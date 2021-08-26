b74dd3b473bc0c39dacfc0c333bf8cca31b15f94
alexey_melnikov <alexantal@yandex.ru>
2021-08-26 17:49:30 +0300

    Initial commit

---
+++ src/main/java/bootjava/restaurantvoting/SbRestaurantApplication.java
@@ -0,0 +1,13 @@
+package bootjava.restaurantvoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRestaurantApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SbRestaurantApplication.class, args);
+  }
+
+}
---
+++ src/test/java/bootjava/restaurantvoting/SbRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package bootjava.restaurantvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbRestaurantApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
