097ca520b8acec49f15c76d1d76cc8c11ee03a16
Alexantal <Alexantal@yandex.ru>
2021-09-17 10:21:41 +0300

    Authorize requests

--- src/main/java/bootjava/restaurantvoting/SbRestaurantApplication.java
+++ src/main/java/bootjava/restaurantvoting/SbRestaurantApplication.java
@@ -1,23 +1,14 @@
 package bootjava.restaurantvoting;
 
-import bootjava.restaurantvoting.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @AllArgsConstructor
-public class SbRestaurantApplication implements ApplicationRunner {
-  private final UserRepository userRepository;
+public class SbRestaurantApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SbRestaurantApplication.class, args);
-  }
-
-  @Override
-  public void run(ApplicationArguments args) {
-    System.out.println(userRepository.findByLastNameContainingIgnoreCase("last"));
   }
 }
--- src/main/java/bootjava/restaurantvoting/config/WebSecurityConfig.java
+++ src/main/java/bootjava/restaurantvoting/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package bootjava.restaurantvoting.config;
 
 import bootjava.restaurantvoting.AuthUser;
+import bootjava.restaurantvoting.model.Role;
 import bootjava.restaurantvoting.model.User;
 import bootjava.restaurantvoting.repository.UserRepository;
 import lombok.AllArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -38,4 +40,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService())
                 .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder());
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/account").hasRole(Role.USER.name())
+                .antMatchers("/api/**").hasRole(Role.ADMIN.name())
+                .and().formLogin();
+    }
+
 }
